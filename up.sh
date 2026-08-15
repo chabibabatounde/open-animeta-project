adde24d2aa6b5d1a6ae9829d5f94a2e2d2f1e720
chabibabatounde <chabibabatounde@gmail.com>
2026-08-16 00:41:21 +0200

    Fist prod

--- up.sh
+++ up.sh
@@ -1,8 +1,9 @@
 git pull
-pm2 stop animeta
-pm2 delete animeta
+
 cp .prod/baseurl.js api/helpers/baseurl.js
 cp .prod/landingpage/script.js assets/js/landingpage/script.js
 cp .prod/datastores.js config/datastores.js
+
+pm2 stop animeta
+pm2 delete animeta
 pm2 start app.js -n animeta -- --port 1919
-# pm2 logs animeta --lines 50
