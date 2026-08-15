4ef3c4b09bd25f1175c4b5af2eb2587ccae7d59f
chabibabatounde <chabibabatounde@gmail.com>
2026-08-15 23:58:06 +0200

    Fist prod

--- up.sh
+++ up.sh
@@ -3,3 +3,4 @@ pm2 stop animeta
 pm2 start app.js -n animeta -- --port 1919
 cp .prod/baseurl.js api/helpers/baseurl.js
 cp .prod/landingpage/script.js assets/js/landingpage/script.js
+cp .prod/datastores.js config/datastores.js
